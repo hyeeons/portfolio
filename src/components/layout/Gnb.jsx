e21f2b468ec093edc8dd4e37643c164f47a09c2a
hyeeons <qouop0101@gmail.com>
2024-02-22 17:01:50 +0900

    gnb 작업

--- src/components/layout/Gnb.jsx
+++ src/components/layout/Gnb.jsx
@@ -2,42 +2,104 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import theme from "../../themes/theme";
 import { FaPagelines } from "react-icons/fa";
-import { FaHospital } from "react-icons/fa";
+import { MdLocalHospital } from "react-icons/md";
 import { MdDashboardCustomize } from "react-icons/md";
 import { DiJavascript } from "react-icons/di";
+import { useState } from "react";
 
 const Gnb = () => {
+  const [isSelected, setIsSelected] = useState(0);
+
   const MenuArr = [
-    { path: "/vita500", icon: <FaPagelines />, text: "비타500 리뉴얼" },
+    {
+      path: "/vita500",
+      icon: <FaPagelines />,
+      text: "비타500 리뉴얼",
+      color: "orangeVita",
+    },
     {
       path: "/samsunghospital",
-      icon: <FaHospital />,
+      icon: <MdLocalHospital />,
       text: "삼성서울병원 리뉴얼",
+      color: "blueSamsung",
     },
 
     {
       path: "/dashboard",
       icon: <MdDashboardCustomize />,
       text: "Dashboard",
+      color: "green",
     },
     {
       path: "/jsdiary",
       icon: <DiJavascript />,
       text: "Javascript Diary",
+      color: "red",
     },
   ];
+
+  const SelectColor = () => {
+    return theme.colors[MenuArr[isSelected].color];
+  };
+
+  const SelectText = (index) => {
+    return {
+      color:
+        isSelected === index
+          ? theme.colors.txtdefault
+          : //   ? SelectColor(index)
+            theme.colors.grayDefault,
+
+      fontWeight: isSelected === index ? "800" : "600",
+      background:
+        isSelected === index
+          ? `linear-gradient(
+		to bottom,
+		transparent 70%,
+		${SelectColor()} 30%`
+          : "",
+    };
+  };
   return (
     <GnbWrap>
       <div className="header">
-        <h1>Portfolio</h1>
+        <h1 style={{ color: SelectColor() }}>Portfolio</h1>
       </div>
       <div className="menuWrap">
         {MenuArr.map((item, index) => (
           <div className="menu-container" key={index}>
-            <Link to={item.path} className="link">
+            <Link
+              to={item.path}
+              className="link"
+              onClick={() => {
+                setIsSelected(index);
+              }}
+            >
               <div className="menu-box">
-                <div className="menu menu-icon">{item.icon}</div>
-                <div className="menu menu-text">{item.text}</div>
+                <div
+                  className="menu menu-icon"
+                  style={SelectText(index)}
+                  //   style={{
+                  //     color:
+                  //       isSelected === index
+                  //         ? SelectColor()
+                  //         : theme.colors.grayDefault,
+                  //   }}
+                >
+                  {item.icon}
+                </div>
+                <div
+                  className="menu menu-text"
+                  style={SelectText(index)}
+                  //   style={{
+                  //     color:
+                  //       isSelected === index
+                  //         ? theme.colors.txtDkNavy
+                  //         : theme.colors.grayDefault,
+                  //   }}
+                >
+                  {item.text}
+                </div>
               </div>
             </Link>
           </div>
@@ -46,6 +108,7 @@ const Gnb = () => {
     </GnbWrap>
   );
 };
+
 const GnbWrap = styled.div`
   background-color: ${theme.colors.grayF9};
   width: 282px;
@@ -64,25 +127,28 @@ const GnbWrap = styled.div`
     font-weight: 700;
     text-align: center;
     padding: 30px 0;
-    color: ${theme.colors.orangeVita};
+
+    /* color: ${theme.colors.orangeVita}; */
   }
   .menuWrap {
-    background: lightblue;
+    /* background: lightblue; */
     width: 237px;
     height: 706px;
-    margin: 40px auto 0;
+    margin: 30px auto 0;
   }
 
   .menu-box {
     height: 40px;
     padding: 0 20px;
     display: flex;
-    gap: 10px;
-    align-items: center;
+    align-items: flex-end;
     cursor: pointer;
+
     .menu {
       font-size: 19px;
-      font-weight: 700;
+    }
+    .menu-icon {
+      padding-right: 7px;
     }
   }
 `;
--- src/themes/theme.js
+++ src/themes/theme.js
@@ -3,11 +3,29 @@ import { createContext } from "react";
 export const theme = {
   colors: {
     orangeVita: "#FD790D",
+    orange: "#ffcc5f",
+    blueSamsung: "#0B50A1",
     grayF9: "#F9F9F9",
+    grayDefault: "#9CA7BA",
     gnbLine: "#827362",
     black: "#000",
     txtdefault: "#222",
-    txtDkNavy: "#2D3748",
+    txtDkNavy: "#384B6B",
+    // 임시
+    green: "#40B16A",
+    red: "red",
+  },
+
+  gray: {
+    100: "#E0E5F2",
+    200: "#E1E9F8",
+    300: "#F4F7FE",
+    400: "#E9EDF7",
+    500: "#8F9BBA",
+    600: "#A3AED0",
+    700: "#707EAE",
+    800: "#707EAE",
+    900: "#1B2559",
   },
   fonts: {
     fz20bold: {
