59b97ecdf19e2231df99caad420144291281136e
hyeeons <qouop0101@gmail.com>
2024-02-24 18:21:24 +0900

    home

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,10 +1,37 @@
+import styled from "styled-components";
+import theme from "../../themes/theme";
+
 const Home = () => {
   return (
     <>
-      <div>메인홈입니다.</div>
-      <div>메인</div>
+      <Box className="box">
+        <p>HELLO!</p>
+      </Box>
     </>
   );
 };
 
+const Box = styled.div`
+  @keyframes boxAnimation {
+    from {
+      border-radius: 150px;
+    }
+    to {
+      border-radius: 0;
+    }
+  }
+
+  background-color: ${theme.colors.orange};
+  margin: 15% auto;
+  width: 308px;
+  height: 308px;
+  animation: boxAnimation 2s linear alternate;
+  p {
+    font-size: 45px;
+    font-weight: bold;
+    text-align: center;
+    margin: 150px auto;
+    color: ${theme.colors.txtdefault};
+  }
+`;
 export default Home;
