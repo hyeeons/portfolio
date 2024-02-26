4a409ca6adca678d34e9cfa359797437a27ef36e
hyeeons <qouop0101@gmail.com>
2024-02-26 18:43:32 +0900

    메인화면 작업중

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,37 +1,65 @@
 import styled from "styled-components";
 import theme from "../../themes/theme";
+import { useEffect } from "react";
+import gsap from "gsap";
 
 const Home = () => {
+  useEffect(() => {
+    const tl = gsap.timeline();
+    tl.fromTo(
+      ".circleBox",
+      { scale: 0 },
+      { scale: 1, borderRadius: "9999px", duration: 1 }
+    );
+    tl.fromTo(".hello", { opacity: 0 }, { opacity: 1 });
+    tl.to(".circleBox", { borderRadius: 0, duration: 1 });
+    tl.to(".circleBox", { rotate: 45 });
+    tl.to(".hello", { rotate: -45 });
+  }, []);
   return (
     <>
+      <CircleBox className="circleBox">
+        <Hello className="hello">HELLO!</Hello>
+      </CircleBox>
+
       <Box className="box">
-        <p>HELLO!</p>
+        <p>
+          Frontend <br />
+          Developer <br /> 이현정입니다
+        </p>
       </Box>
     </>
   );
 };
 
-const Box = styled.div`
-  @keyframes boxAnimation {
-    from {
-      border-radius: 150px;
-    }
-    to {
-      border-radius: 0;
-    }
-  }
+const Hello = styled.h1`
+  font-size: 38px;
+  font-weight: bold;
+  text-align: center;
+  color: ${theme.colors.txtdefault};
+`;
 
+const CircleBox = styled.div`
+  width: 250px;
+  height: 250px;
   background-color: ${theme.colors.orange};
   margin: 15% auto;
-  width: 308px;
-  height: 308px;
-  animation: boxAnimation 2s linear alternate;
-  p {
-    font-size: 45px;
-    font-weight: bold;
-    text-align: center;
-    margin: 150px auto;
-    color: ${theme.colors.txtdefault};
+  h1 {
+    padding: 105px 0;
   }
 `;
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: ${theme.colors.orange};
+  opacity: 0;
+  position: absolute;
+  top: 34%;
+  left: 45%;
+  h1 {
+    padding: 75px 0;
+  }
+`;
+
 export default Home;
