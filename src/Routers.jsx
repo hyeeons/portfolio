458afda737ec7855cf9d2f0ca9d1cfceff86541e
hyeeons <qouop0101@gmail.com>
2024-03-10 22:19:09 +0900

    라우터 basename 수정

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -12,7 +12,7 @@ import Portfolio from "./views/portfolio/Portfolio";
 const Routers = () => {
   return (
     <>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
