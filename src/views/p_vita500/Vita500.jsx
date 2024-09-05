087c001315bef513ff7fcd77aa8aceac2808e1ea
hyeeons <qouop0101@gmail.com>
2024-09-05 12:37:14 +0900

    비타500 프로젝트 설명 수정

--- src/views/p_vita500/Vita500.jsx
+++ src/views/p_vita500/Vita500.jsx
@@ -30,13 +30,13 @@ const Vita500 = () => {
         title="비타500 리뉴얼 사이트"
         subtitle="광동제약의 비타500 브랜드 소개 페이지"
         subText="100% 개인작업"
-        siteType="PC & Mobile"
-        pageType="메인페이지"
-        pageText="사이트 리뉴얼 기획/디자인 및 사이트 제작"
+        siteType="PC & Mobile 메인페이지"
+        // pageType="메인페이지"
+        pageText="비타500이 가진 상큼한 이미지를 중점으로 하여 다양한 비타500의 제품, 홍보자료 등을 소개하는 페이지입니다"
         website="https://hyeeons.github.io/vita500/"
         github="https://github.com/hyeeons/vita500"
         figma="https://www.figma.com/file/4HJHz7EWSDft9uy5ZpWqvv/%EA%B4%91%EB%8F%99%EC%A0%9C%EC%95%BD_%EB%B9%84%ED%83%80500-%EB%A6%AC%EB%89%B4%EC%96%BC%EC%82%AC%EC%9D%B4%ED%8A%B8?type=design&node-id=0%3A1&mode=design&t=QQ9MjlkYaBsY2r1A-1"
-        projectComment="광동제약의 비타500 제품 브랜드 소개 페이지입니다. HTML과 CSS를 활용하여 제작한 첫 사이트이며, 브랜드가 가지고 있는 상큼한 이미지를 표현하기 위해 일러스트를 추가했습니다. 또한 비타민이 차오르는 느낌을 주기 위하여 메인 제품 소개의 배경을 그라데이션으로 표현하고 스크롤에 따라 콘텐츠가 등장하게 함으로써 동적인 표현을 추가하였습니다. 또한 비타500의 콘텐츠와 다양한 제품을 소개하여 브랜드 소개에 중점을 두었습니다."
+        projectComment="광동제약의 비타500은 오랜 시간동안 많은 사람들에게 사랑을 받고 있는 브랜드입니다. 이러한 브랜드가 가지고 있는 상큼한 이미지를 표현하기 위해 일러스트를 추가하였습니다. 또한, 비타민이 차오르는 느낌을 주기 위해 메인 제품 소개 배경을 그라데이션으로 적용하고 스크롤에 따라 제품이 등장하는 동적인 화면을 구현했습니다. 비타500 병음료와 달리 상대적으로 덜 알려진 다른 제품군을 위해 비타500의 다양한 제품을 소개하는 섹션을 추가하였으며, 다양한 홍보자료도 볼 수 있도록 기획함으로써 브랜드 소개에 중점을 두었습니다. HTML과 SCSS를 활용하여 제작한 첫 사이트이며, 유일하게 모바일을 먼저 작업한 후 PC버전을 제작한 사이트입니다. "
         bgImg={bgVita500}
       />
     </>
