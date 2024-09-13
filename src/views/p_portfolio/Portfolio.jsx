aca6f175ebc415dc28b0690c442d1548fcac19eb
hyeeons <qouop0101@gmail.com>
2024-09-13 15:34:59 +0900

    포트폴리오 - 스킬 수정

--- src/views/p_portfolio/Portfolio.jsx
+++ src/views/p_portfolio/Portfolio.jsx
@@ -14,14 +14,7 @@ const Portfolio = () => {
 		alt:"Router",
 		src: "https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white"
 	},
-	{
-		alt:"HTML5",
-		src: "https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white"
-	},
-	{
-		alt:"CSS3",
-		src: "https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"
-	},
+	
 	{
 		alt:"Javascript",
 		src: "https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=JavaScript&logoColor=white"
