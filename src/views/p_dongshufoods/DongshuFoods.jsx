8689068868db81e7c07fb4f49b63d910afa866ea
hyeeons <qouop0101@gmail.com>
2024-09-13 21:06:00 +0900

    피그마 주소

--- src/views/p_dongshufoods/DongshuFoods.jsx
+++ src/views/p_dongshufoods/DongshuFoods.jsx
@@ -34,7 +34,7 @@ const DongshuFoods = () => {
 			pageText="동서식품의 다양한 브랜드와 제품, 홍보자료를 소개하는 동서식품 리뉴얼 사이트"
 			website="https://hyeeons.github.io/dongsuhfoods/"
 			github="https://github.com/hyeeons/dongsuhfoods"
-			figma=""
+			figma="https://www.figma.com/design/bdX4bCrRepOuoEQVIndbH0/%EB%8F%99%EC%84%9C%EC%8B%9D%ED%92%88-%EB%A6%AC%EB%89%B4%EC%96%BC-%EC%82%AC%EC%9D%B4%ED%8A%B8"
 			projectComment="동서식품은 많은 사람들에게 알려진 맥심커피, 카누, 현미녹차, 오레오, 제티 등 다양한 제품을 생산하고 판매하는 식품회사입니다. '생활 속에 향기를 더하는 기업'이라는 동서식품의 이념을 나타내며 우리가 일상생활에서 함께하는 다양한 동서식품의 브랜드와 제품을 소개하고 있습니다. 또한, 동서식품의 다양한 프로젝트인 맥심플랜트, 사회공헌 활동들, 웹진 삶의향기, 커피클래스 등을 소개하고 있습니다. 해당 부분은 사용자 환경 특성을 고려하여 PC버전에서는 탭 형식으로, 모바일 버전에서는 탭이 아닌 스크롤에 따라 항목이 등장하게 작업하였고 HTML 코드를 중복하지 않게 하기 위해 AOS를 커스터마이징하여 작업하였습니다."
 			bgImg={bg_dongsuhfoods}
 		  />
--- src/views/p_portfolio/Portfolio.jsx
+++ src/views/p_portfolio/Portfolio.jsx
@@ -38,7 +38,7 @@ const Portfolio = () => {
         pageType="사용자가 프로젝트를 한 눈에 볼 수 있도록 제작하였으며, 프로젝트의 관리가 용이한 포트폴리오 사이트입니다."
         website="https://hyeeons.github.io/portfolio/"
         github="https://github.com/hyeeons/portfolio"
-        figma="https://www.figma.com/file/jpFsCMMhbWoSa8f38jh3lU/%ED%8F%AC%ED%8A%B8%ED%8F%B4%EB%A6%AC%EC%98%A4(React%ED%99%9C%EC%9A%A9)?type=design&node-id=0%3A1&mode=design&t=jIueZ0rryuS4TiTT-1"
+        figma="https://www.figma.com/design/lesJxoDlbWKC1CCTVcRDTH/%EC%9B%B9%ED%8D%BC%EB%B8%94%EB%A6%AC%EC%85%94-%ED%8F%AC%ED%8A%B8%ED%8F%B4%EB%A6%AC%EC%98%A4-(React)?node-id=0-1&t=j9Prg9tD3x5RyeNp-1"
         projectComment="사용자의 입장에서 좋은 포트폴리오란 무엇일까 생각했습니다. 포트폴리오는 여러가지 프로젝트를 소개하는 특성을 가지고 있습니다. 이러한 점을 고려하여 직관적이고 프로젝트의 관리가 편리한 사이트를 기획했습니다. 이를 위해 iframe을 활용하여 사이트 내에서 여러가지 프로젝트들을 한 눈에 볼 수 있도록 제작하였으며, React Router를 활용하여 페이지 이동을 편리하게 하였습니다. 또한, useState와 useEffect와 같은 Hook을 사용하여 프로젝트마다 메뉴 색상 등을 변경함으로써 프로젝트 분위기에 어울리는 동적인 페이지를 구현했습니다.	"
 		
         bgImg={bgPortfolio}
